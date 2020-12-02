8c093ab46e0d61fdb4e6607f3f0b22b33d45d801
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-03 02:01:01 +0530

    Added : welcome message

---
+++ src/main/java/com/findmaxgenerics/FindMax.java
@@ -0,0 +1,7 @@
+package com.findmaxgenerics;
+
+public class FindMax {
+    public static void main(String[] args) {
+        System.out.println("****** Welcome to arithmetic number computation ******");
+    }
+}
